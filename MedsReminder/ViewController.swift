7f87aba189fbb6fd4fb3b028ef0bc29142501a00
SajedeNouri <Sajoo@sajoo-macbook-air.local>
2017-08-31 11:46:58 +0430

    Initial Commit

---
+++ MedsReminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedsReminder
+//
+//  Created by SajedeNouri on 6/9/1396 AP.
+//  Copyright © 1396 AP SajedeNouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
