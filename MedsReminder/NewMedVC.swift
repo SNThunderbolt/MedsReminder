97f42e39dcac6bfb7d2323b92b1159594a17eaca
SNThunderbolt <loadstar.saji@gmail.com>
2017-09-01 21:33:59 +0430

    design almost completed...

---
+++ MedsReminder/NewMedVC.swift
@@ -0,0 +1,35 @@
+//
+//  NewMedVC.swift
+//  MedsReminder
+//
+//  Created by SajedeNouri on 6/10/1396 AP.
+//  Copyright © 1396 AP SajedeNouri. All rights reserved.
+//
+
+import UIKit
+
+class NewMedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
